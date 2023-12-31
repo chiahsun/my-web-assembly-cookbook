1290e4d5cdb520f47e218f86d643a868a69f4d04
chiahsun <chiahsun0814@gmail.com>
2023-12-31 22:37:09 +0800

    Add cwrap example using sqrt

--- 1.1-CompileCModuleToWebAssembly/1.1.3-CallCustom/hello3.c
+++ 1.1-CompileCModuleToWebAssembly/1.1.3-CallCustom/hello3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <emscripten/emscripten.h>
+#include <math.h>
 
 int main() {
     printf("Hello World from Hello 3\n");
@@ -16,3 +17,7 @@ EXTERN EMSCRIPTEN_KEEPALIVE void myFunction(int argc, char** argv) {
     printf("My Function Called\n");
 }
 
+// https://emscripten.org/docs/porting/connecting_cpp_and_javascript/Interacting-with-code.html#calling-compiled-c-functions-from-javascript-using-ccall-cwrap
+EXTERN EMSCRIPTEN_KEEPALIVE int int_sqrt(int x) {
+    return sqrt(x);
+}
---
+++ 1.1-CompileCModuleToWebAssembly/1.1.3-CallCustom/sqrt.js
@@ -0,0 +1,5 @@
+var int_sqrt = Module.cwrap("int_sqrt", "number", ["number"]);
+// var myNumber = Module.ccall("int_sqrt", // name of C function
+//                    "number", // return type
+//                    ["number"], // argument types
+//                    [28]);
